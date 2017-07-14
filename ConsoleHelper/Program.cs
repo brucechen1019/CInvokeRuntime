9535ed24471f9b443b126979fbd7516260775994
brucechen1019 <lin.chen1019@hotmail.com>
2017-07-14 18:13:07 +0800

    init

---
+++ ConsoleHelper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHelper
+{
+    class Program
+    {
+        public sealed class Assist
+        {
+            private static readonly Assist _instance = new Assist();
+            private AssistResponseDelegate _del;
+            public static Assist Instance { get { return _instance; } }
+
+            public delegate void AssistResponseDelegate(AssistResponse r);
+
+            public void Pub(AssistResponseDelegate del)
+            {
+                _del = (AssistResponseDelegate)Delegate.Combine(_del, del);
+            }
+
+            public void InvokeDemo()
+            {
+                var r = new AssistResponse() { testresponse = "test" };
+                _del?.Invoke(r);
+            }
+
+            public class AssistResponse
+            {
+                public string testresponse { get; set; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Assist.Instance.Pub((s) =>
+            {
+                Console.WriteLine("client1:" + s.testresponse);
+            });
+
+            Assist.Instance.Pub((s) =>
+            {
+                Console.WriteLine("client2:" + s.testresponse);
+            });
+
+
+            Assist.Instance.InvokeDemo();
+        }
+    }
+}
---
+++ SampleComponent/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleComponent
+{
+
+    //public delegate void AssistResponseEventHandler<TEventArgs>(object sender, TEventArgs e);
+
+    public sealed class Assist
+    {
+        /*
+         * reference: 
+         *  https://msdn.microsoft.com/en-us/library/hh972883.aspx
+         *  https://docs.microsoft.com/en-us/windows/uwp/winrt-components/walkthrough-creating-a-simple-windows-runtime-component-and-calling-it-from-javascript#prerequisites
+         */
+
+        private EventRegistrationTokenTable<EventHandler<AssistResponse>> onAssistResponseTokenTable = null;
+
+        #region Instance
+        private static readonly Assist _instance = new Assist();
+        public static Assist Instance { get { return _instance; } }
+        #endregion
+
+        #region Event
+        public event EventHandler<AssistResponse> OnAssistResponsed
+        {
+            add
+            {
+                return EventRegistrationTokenTable<EventHandler<AssistResponse>>
+                    .GetOrCreateEventRegistrationTokenTable(ref onAssistResponseTokenTable)
+                    .AddEventHandler(value);
+            }
+            remove
+            {
+                EventRegistrationTokenTable<EventHandler<AssistResponse>>
+                    .GetOrCreateEventRegistrationTokenTable(ref onAssistResponseTokenTable)
+                    .RemoveEventHandler(value);
+            }
+        }
+        #endregion
+
+        void OnAssistResponse(AssistResponse arg)
+        {
+            EventRegistrationTokenTable<EventHandler<AssistResponse>>
+                .GetOrCreateEventRegistrationTokenTable(ref onAssistResponseTokenTable)
+                .InvocationList?.Invoke(this, arg);
+        }
+
+        public static string SendAssistResponse()
+        {
+            //OnAssistResponse(new AssistResponse()
+            //{
+            //    testresponse = DateTime.UtcNow.ToString()
+            //});
+            return "hello world";
+        }
+    }
+
+    public sealed class AssistResponse
+    {
+        public string testresponse { get; set; }
+    }
+}
